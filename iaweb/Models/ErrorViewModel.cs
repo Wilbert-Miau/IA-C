4bc73ce1db31fbfefe3ce89298128c3f92f06e61
3GATO3 <106358166+3GATO3@users.noreply.github.com>
2022-11-03 17:07:37 -0600

    Delete iaweb directory

--- iaweb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace iaweb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
